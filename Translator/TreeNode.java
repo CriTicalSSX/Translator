5e6c9731e13d16d1f3d4dee05e7f8b60aa413af6
ambialba <willlockett13@gmail.com>
2017-03-13 14:50:00 +0000

    Add files via upload
    Minor edits to the commenting, mostly just me working out how to use github

--- Translator/TreeNode.java
+++ Translator/TreeNode.java
@@ -10,6 +10,21 @@ public class TreeNode
     private String engWord, gerWord;
     private TreeNode left, right;
 
+    /**
+     * default constrctor for tree node objects
+     */
+    public TreeNode()
+    {
+        id = 0;
+        engWord = "";
+        gerWord = "";
+        left = null;
+        right = null;
+    }
+    
+    /**
+     * alternate constructor for tree node objects that fills in fields according to users input
+     */
     public TreeNode(int id, String engWord, String gerWord)
     {
         this.id = id;
@@ -29,46 +44,91 @@ public class TreeNode
         return id;
     }
     
+    /**
+     * Sets the ID contained in this node
+     * 
+     * @param id The unique ID of the word
+     */
     public void setID(int id)
     {
         this.id = id;
     }
     
+    /**
+     * Gets the english word stored in the node
+     * 
+     * @return engWord The english word stored in the node
+     */
     public String getEngWord()
     {
         return engWord;
     }
     
+    /**
+     * Sets the english word stored in the node
+     * 
+     * @param engWord The english word to be stored in the node
+     */
     public void setEngWord(String engWord)
     {
         this.engWord = engWord;
     }
     
+    /**
+     * Gets the german word stored in the node
+     * 
+     * @return gerWord The german word stored in the node
+     */
     public String getGerWord()
     {
         return engWord;
     }
     
+    /**
+     * Sets the german word to be stored in the node
+     * 
+     * @param gerWord The german word stored in the node
+     */
     public void setGerWord(String gerWord)
     {
         this.gerWord = gerWord;
     }
     
+    /**
+     * Gets the memory adress of the left child of the node
+     * 
+     * @return left The left child of the current node
+     */
     public TreeNode getLeft()
     {
         return left;
     }
     
+    /**
+     * Sets the memory adress of the left child of the node
+     * 
+     * @param left The left child of the current node
+     */
     public void setLeft(TreeNode left)
     {
         this.left = left;
     }
     
+    /**
+     * Gets the memory adress of the right child of the node
+     * 
+     * @return right The right child of the current node
+     */
     public TreeNode getRight()
     {
         return right;
     }
     
+    /**
+     * Sets the memory adress of the right child of the node
+     * 
+     * @param right The right child of the current node
+     */
     public void setRight(TreeNode right)
     {
         this.right = right;
